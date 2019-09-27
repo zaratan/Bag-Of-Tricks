68e57d1003f469bcf7d85505f175a3ad1940793a
Denis Zaratan Pasin <denis@pasin.fr>
2019-09-28 07:22:37 +0900

    Better config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.jsx"
+    popup: './src/popup.jsx',
+  },
+  output: {
+    filename: '[name].[contenthash].bundle.js'
   },
   resolve: {
-    extensions: [".jsx", ".js"]
+    extensions: ['.jsx', '.js']
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
+      template: 'src/popup.html',
+      filename: 'popup.html',
       inject: true,
-      chunks: ["index"]
+      chunks: ['popup']
     }),
-    new CopyPlugin([{ from: "manifest.json", to: "." }])
+    new ManifestPlugin({
+      seed: {
+        description: 'Simple tricks to help your life.',
+        manifest_version: 2,
+        name: 'Bag of Tricks',
+        version: '0.0.2',
+        browser_action: {
+          default_popup: 'popup.html'
+        },
+        permissions: ['storage', 'activeTab']
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
   ],
   module: {
     rules: [
@@ -24,14 +54,24 @@ module.exports = {
         test: /\.m?jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
-  mode: "production"
+  mode: 'production',
 };
